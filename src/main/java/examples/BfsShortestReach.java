acb3d6634999fc62180969c7e9ae775b401af25a
Anish Hirlekar <anish.hirlekar@metamug.com>
2022-02-11 16:30:45 +0530

    First

---
+++ src/main/java/examples/BfsShortestReach.java
@@ -0,0 +1,146 @@
+package examples;
+
+import java.util.*;
+
+/*
+Input Format
+
+The first line contains an integer, q, the number of queries.
+
+Each of the following q sets of lines is as follows:
+
+The first line contains two space-separated integers, n and m, the number of nodes and the number of edges.
+Each of the next m lines contains two space-separated integers, u and v, describing an edge connecting node  to node .
+The last line contains a single integer, s, the index of the starting node.
+
+Output Format
+
+For each of the queries, print a single line of space-separated integers denoting the shortest distances to each
+of the  other nodes from starting position. These distances should be listed sequentially by node number (i.e., ),
+but should not include node . If some node is unreachable from , print  as the distance to that node.
+
+Sample Input
+
+2
+4 2
+1 2
+1 3
+1
+3 1
+2 3
+2
+ */
+public class BfsShortestReach {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int numberOfQueries = sc.nextInt(); //first line is number of queries
+        System.out.println("Number of queries: "+numberOfQueries);
+        for(int query=0; query < numberOfQueries; query++) {
+
+            System.out.println("For query "+query);
+            sc.nextLine();
+            int nodes = sc.nextInt();
+            int edges = sc.nextInt();
+
+            System.out.println("Nodes: "+nodes);
+            System.out.println("Edges: "+edges);
+            Graph g = new Graph(nodes);
+
+            for(int edge=0; edge < edges; edge++) {
+                sc.nextLine();
+
+                System.out.println("For Edge "+edge);
+                int edgeStartPoint = sc.nextInt();
+                int edgeEndPoint = sc.nextInt();
+                System.out.println("Edge St: "+edgeStartPoint);
+                System.out.println("Edge end: "+edgeEndPoint);
+                g.addEdge(edgeStartPoint,edgeEndPoint);
+            }
+            sc.nextLine();
+
+            System.out.println("Enter starter node: ");
+            int starterNode = sc.nextInt(); // last line
+            g.setStarterNode(starterNode);
+
+            g.printOutput();
+
+            System.out.println();
+        }
+    }
+}
+
+
+class Graph {
+    private int[] outputArray; // holds distances of n-1 nodes apart from start node
+    private Map<Integer,List<Integer>> nodeEdgeMap = new HashMap<>(); // holds List of edge endpoints for a node
+    int starterNode;
+    private final int edgeLength = 6;
+    int numberOfNodes;
+
+    public Graph(int numberOfNodes) {
+        this.numberOfNodes = numberOfNodes;
+        outputArray = new int[numberOfNodes-1];
+        Arrays.fill(outputArray, -1); // fill all positions with -1
+    }
+
+    public void setStarterNode(int s) {
+        starterNode = s;
+    }
+
+    public void addEdge(int edgeStartPt, int edgeEndPt){
+        List<Integer> edges;
+        if (nodeEdgeMap.containsKey(edgeStartPt) && nodeEdgeMap.get(edgeStartPt) != null){
+            edges = nodeEdgeMap.get(edgeStartPt);
+        } else {
+            edges = new ArrayList<>();
+        }
+        edges.add(edgeEndPt);
+        nodeEdgeMap.put(edgeStartPt,edges);
+
+        printNodeEdgeMap();
+    }
+
+    public void printNodeEdgeMap(){
+        //System.out.println();
+        //System.out.println("NODE EDGE MAP: ");
+        for(Integer node : nodeEdgeMap.keySet()){
+          //  System.out.print("Node: "+node+", {");
+            List<Integer> edges = nodeEdgeMap.get(node);
+            for(Integer edge: edges) {
+            //    System.out.print("["+edge+"]");
+            }
+            //System.out.print("}");
+            //System.out.println();
+        }
+        //System.out.println();
+    }
+
+    public int[] createOutput(int starterNode, int edgeLength, int[] outputArray) {
+        // 1. Get edges for starter node
+        //printNodeEdgeMap();
+        if(!nodeEdgeMap.containsKey(starterNode)){
+            return outputArray; // no edge starting with current node
+        }
+        System.out.println("For Starter Node: "+starterNode);
+        List<Integer> starterNodeEdges = nodeEdgeMap.get(starterNode);
+
+        // 2. Fill output Array with edgeDistance for endnodes of starter node edges
+        for (Integer endNode : starterNodeEdges) {
+            outputArray[endNode - 2] = edgeLength;
+            System.out.println("outputArray: "+Arrays.toString(outputArray));
+            outputArray = createOutput(endNode, edgeLength+this.edgeLength, outputArray);
+        }
+        return outputArray;
+    }
+
+    public void printOutput() {
+        int[] outputArray = createOutput(starterNode, edgeLength, this.outputArray);
+
+        // 4. Update distance of
+        System.out.println("Distance array: ");
+        for(int i=0; i< outputArray.length; i++){
+            System.out.print(outputArray[i]+" ");
+        }
+    }
+}
---
+++ src/main/java/examples/Dijkstra.java
@@ -0,0 +1,74 @@
+package examples;
+/*
+    Use array for holding distance values for index i which represents vertex
+
+ */
+public class Dijkstra {
+    // A utility function to print the constructed distance array
+    private static void printSolution(int[] dist)
+    {
+        System.out.println("Vertex \t\t Distance from Source");
+        for (int i = 0; i < dist.length; i++)
+            System.out.println(i + " \t\t " + dist[i]);
+    }
+    public static void main(String[] args) {
+        // create graph array from graph - each element array represents each vertex
+        // each element inside array represents distance of directly connecting vertex from index vertex
+        // represent non-connecting nodes by 0
+        int[][] graph = {
+                {0, 4, 0, 0, 0, 0, 0, 8, 0},
+                { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+                { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+                { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+                { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+                { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+                { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+                { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+                { 0, 0, 2, 0, 0, 0, 6, 7, 0 }
+        };
+        int numberOfVertices = 9;
+
+        int[] dist = new int[numberOfVertices]; // dist will hold distance of each vertex from source vertex
+
+        boolean[] visitedVertices = new boolean[numberOfVertices]; // holds true if vertex has been visited
+
+        // Initialize all distances as INFINITE and stpSet[] as false
+        for (int i = 0; i < numberOfVertices; i++) {
+            dist[i] = Integer.MAX_VALUE;
+            visitedVertices[i] = false;
+        }
+        dist[0] = 0; // for source vertex, dist is always 0
+
+
+        for(int i=0; i<numberOfVertices; i++){
+            // 1. pick the closest vertex from the set of vertices which are NOT YET PROCESSED
+
+            int closestVertex = -1;
+            int closestVertexDistance = Integer.MAX_VALUE;
+            for(int vertex=0; vertex < numberOfVertices; vertex++){
+                if(!visitedVertices[vertex] && dist[vertex] < closestVertexDistance) {
+                    closestVertexDistance = dist[vertex];
+                    closestVertex = vertex;
+                }
+            }
+
+            // update vertex as visited
+            visitedVertices[closestVertex] = true;
+
+            // 2. Update dist value of the adjacent vertices of the
+            // picked vertex.
+            for (int v = 0; v < numberOfVertices; v++) {
+                // Update dist[v] only if is not in sptSet && there is an
+                // edge from u to v && dist of picked vertex from source is not infinity
+                // && and total weight of path from src to v through u is smaller than current value of dist[v]
+                if(!visitedVertices[v] && graph[closestVertex][v] != 0
+                        && dist[closestVertex] != Integer.MAX_VALUE
+                                && dist[closestVertex] + graph[closestVertex][v] < dist[v] ){
+                    dist[v] = dist[closestVertex] + graph[closestVertex][v];
+                }
+            }
+        }
+
+        printSolution(dist);
+    }
+}
