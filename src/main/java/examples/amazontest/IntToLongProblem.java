1dbffc3a33c7ed5ba7566139d4a656c824ce7f91
Anish Hirlekar <anish.hirlekar@metamug.com>
2022-02-15 08:00:18 +0530

    Added problem for testing int size from Amazon online assignment

---
+++ src/main/java/examples/amazontest/IntToLongProblem.java
@@ -0,0 +1,25 @@
+package examples.amazontest;
+
+import java.util.Scanner;
+
+public class IntToLongProblem {
+
+    public static long mod = 1000000007L;
+
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        long totalPower = 0;
+
+        int power = 2147483647;
+
+        for(int i=0;i<t;i++){
+            totalPower+=power;
+            System.out.println("TotalP: "+totalPower);
+            int m = (int) (totalPower % mod);
+
+            System.out.println("M: "+m);
+        }
+    }
+}
